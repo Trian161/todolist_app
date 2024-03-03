b25d20f811fc1b3b65df3126dcc7e727782a5157
Trian161 <trians644@gmail.com>
2024-03-03 11:44:40 +0700

    refactor: rename homepage.dart to home.dart and remove unused component import (custom_bottom_navbar.dart)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:todolist_app/components/custom_app_bar.dart';
-import 'package:todolist_app/components/custom_bottom_navbar.dart';
 import 'package:todolist_app/components/goal_container.dart';
 import 'package:todolist_app/components/searchbar.dart';
 import 'package:todolist_app/components/task_container.dart';
