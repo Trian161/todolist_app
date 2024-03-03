1205e34a3160f3ff3ad0226ae2a79fde4dbe78a8
Trian161 <trians644@gmail.com>
2024-03-03 11:17:00 +0700

    feat: change Convex_bottom_bar to BottomAppBar and use floatingActionButton

--- lib/components/custom_bottom_navbar.dart
+++ lib/components/custom_bottom_navbar.dart
@@ -1,5 +1,6 @@
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:todolist_app/pages/home.dart';
+import 'package:todolist_app/pages/task.dart';
 
 class CustomBottomBar extends StatefulWidget {
   const CustomBottomBar({super.key});
@@ -9,17 +10,73 @@ class CustomBottomBar extends StatefulWidget {
 }
 
 class _CustomBottomBarState extends State<CustomBottomBar> {
+  List<Widget> _pages = [
+    Home(),
+    Task(),
+  ];
+  int _selectedIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ConvexAppBar(
-        style: TabStyle.react,
-        items: [
-          TabItem(icon: Icons.home),
-          TabItem(icon: Icons.task),
-          TabItem(icon: Icons.add),
-          TabItem(icon: Icons.calendar_month),
-          TabItem(icon: Icons.person),
-        ],
-        onTap: (int i) => print('click index=$i'));
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xffffd700),
+        shape: CircleBorder(),
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 50,
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffd6b4fc),
+        ),
+        child: BottomAppBar(
+          height: kBottomNavigationBarHeight * 1.1,
+          color: Color(0xffffd700),
+          elevation: 0,
+          shape: CircularNotchedRectangle(),
+          notchMargin: 7,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.home),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.task),
+                // padding: EdgeInsets.only(right: 30),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(null),
+                // padding: EdgeInsets.only(right: 30),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.calendar_month_rounded),
+                // padding: EdgeInsets.only(left: 30),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
